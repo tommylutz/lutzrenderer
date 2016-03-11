5ab5eb56201033ae2a119843477f60cb10e3af83
Tom Lutz <tommylutz@gmail.com>
2016-03-11 08:19:15 -0500

    Added HorizontalLineTest
    This will test that a horizontal line more than 1 pixel wide will
    properly set the corresponding pixels in the image. The test does *not*
    pass right now. As recommended in Kent Beck's book on unit testing, I
    will end this coding session with a broken test.

--- test/rend.t.cpp
+++ test/rend.t.cpp
@@ -16,3 +16,15 @@ TEST(RendererLineTest, SinglePixelTest)
     rend.draw_line(img,1,2,3,4,255);
 }
 
+TEST(RendererLineTest, HorizontalLineTest)
+{
+    MockImage img;
+    EXPECT_CALL(img, set_pixel(1,5,255)).Times(1);
+    EXPECT_CALL(img, set_pixel(2,5,255)).Times(1);
+    Renderer rend;
+    rend.draw_line(img,
+                   1,5, 
+                   2,5,
+                   255);
+}
+
