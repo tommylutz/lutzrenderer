8b23daefcdc537aede8bd402dc21e328488291b3
Tom Lutz <tommylutz@gmail.com>
2016-03-12 18:30:19 -0500

    Needed a more fun sample image for main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -7,15 +7,22 @@ int main(int argc, char ** argv)
     std::cout << "Hello world\n";
     libpng_Interface i_libpng;
     file_Interface i_file;;
-    PngImage img(100,100, &i_libpng, &i_file);
+    PngImage img(1000,1000, &i_libpng, &i_file);
     Renderer rend;
-    rend.draw_line(img, 0,  0,  99, 99, 255);
-    rend.draw_line(img, 99, 0,  0,  99, 255);
+    
+    for(int x=0; x<1000; x+=10)
+    {
+        rend.draw_line(img, x, 0, 999, x, abs(255-x/2)); 
+        rend.draw_line(img, 0, x, x, 999, abs(255-x/2)); 
+    }
 
-    rend.draw_line(img, 0,  0,  0,  99, 255);
-    rend.draw_line(img, 0,  99, 99, 99, 255);
-    rend.draw_line(img, 99, 99, 99, 0,  255);
-    rend.draw_line(img, 99, 0,  0,  0,  255);
+    rend.draw_line(img, 0,   0,  999, 999, 255);
+    rend.draw_line(img, 999, 0,  0,   999, 255);
+
+    rend.draw_line(img, 0,   0,   0,   999, 255);
+    rend.draw_line(img, 0,   999, 999, 999, 255);
+    rend.draw_line(img, 999, 999, 999, 0,   255);
+    rend.draw_line(img, 999, 0,   0,   0,   255);
 
     img.write("./foo.png");
     return 0;
