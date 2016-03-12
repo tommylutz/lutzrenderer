af84f4818d3d0692ccc92604318325bb8bd2db44
Tom Lutz <tommylutz@gmail.com>
2016-03-12 10:33:23 -0500

    Basic two-pixel lines all tested

--- librend/rend.cpp
+++ librend/rend.cpp
@@ -1,6 +1,8 @@
 #include <rend.h>
 #include <iostream>
 #include "image_interface.h"
+#include <math.h>
+#include <stdlib.h>
 
 Renderer::Renderer()
 {
@@ -15,7 +17,32 @@ void Renderer::draw_line(ImageInterface &img,
                          int x1, int y1,
                          int graylevel)
 {
-    img.set_pixel(x0,y0,graylevel);
+    bool swapped = false;
+    if(abs(x0-x1) < abs(y0-y1))
+    {
+        std::swap(x0,y0);
+        std::swap(x1,y1);
+        swapped = true;
+    }
 
+    if(x0 > x1)
+    {
+        std::swap(x0,x1);
+        std::swap(y0,y1);
+    }
+
+    float pct_done = 0;
+    const float y_total_dist = ((float)(y1-y0));
+    const float x_total_dist = ((float)(x1-x0));
+
+    for(int x=x0; x<=x1; ++x)
+    {
+        pct_done = x_total_dist?(((float)(x-x0))/x_total_dist):0;
+        int y = round(y0+pct_done*y_total_dist);
+        if(swapped)
+            img.set_pixel(y,x,graylevel);
+        else
+            img.set_pixel(x,y,graylevel);
+    }
 }
 
--- test/rend.t.cpp
+++ test/rend.t.cpp
@@ -13,7 +13,7 @@ TEST(RendererLineTest, SinglePixelTest)
     EXPECT_CALL(img, set_pixel(1,2,255)).Times(1);
     
     Renderer rend;
-    rend.draw_line(img,1,2,3,4,255);
+    rend.draw_line(img,1,2,1,2,255);
 }
 
 TEST(RendererLineTest, HorizontalLineTest)
@@ -28,3 +28,86 @@ TEST(RendererLineTest, HorizontalLineTest)
                    255);
 }
 
+TEST(RendererLineTest, BackwardsHorizontalLineTest)
+{
+    MockImage img;
+    EXPECT_CALL(img, set_pixel(1,5,255)).Times(1);
+    EXPECT_CALL(img, set_pixel(2,5,255)).Times(1);
+    Renderer rend;
+    rend.draw_line(img,
+                   2,5, 
+                   1,5,
+                   255);
+}
+
+TEST(RendererLineTest, DiagonalLineTestBottomLeftToTopRight)
+{
+    MockImage img;
+    EXPECT_CALL(img, set_pixel(2,4,255)).Times(1);
+    EXPECT_CALL(img, set_pixel(1,3,255)).Times(1);
+    Renderer rend;
+    rend.draw_line(img,
+                   1,3, 
+                   2,4,
+                   255);
+}
+
+TEST(RendererLineTest, DiagonalLineTestTopLeftToBottomRight)
+{
+    MockImage img;
+    EXPECT_CALL(img, set_pixel(2,9,255)).Times(1);
+    EXPECT_CALL(img, set_pixel(3,8,255)).Times(1);
+    Renderer rend;
+    rend.draw_line(img,
+                   2,9, 
+                   3,8,
+                   255);
+}
+
+TEST(RendererLineTest, DiagonalLineTestTopRightToBottomLeft)
+{
+    MockImage img;
+    EXPECT_CALL(img, set_pixel(3,9,255)).Times(1);
+    EXPECT_CALL(img, set_pixel(2,8,255)).Times(1);
+    Renderer rend;
+    rend.draw_line(img,
+                   3,9, 
+                   2,8,
+                   255);
+}
+
+TEST(RendererLineTest, DiagonalLineTestBottomRightToTopLeft)
+{
+    MockImage img;
+    EXPECT_CALL(img, set_pixel(3,9,255)).Times(1);
+    EXPECT_CALL(img, set_pixel(2,8,255)).Times(1);
+    Renderer rend;
+    rend.draw_line(img,
+                   3,9, 
+                   2,8,
+                   255);
+}
+
+TEST(RendererLineTest, VerticalLineTestTopToBottom)
+{
+    MockImage img;
+    EXPECT_CALL(img, set_pixel(2,9,255)).Times(1);
+    EXPECT_CALL(img, set_pixel(2,8,255)).Times(1);
+    Renderer rend;
+    rend.draw_line(img,
+                   2,9, 
+                   2,8,
+                   255);
+}
+
+TEST(RendererLineTest, VerticalLineTestBottomToTop)
+{
+    MockImage img;
+    EXPECT_CALL(img, set_pixel(2,8,255)).Times(1);
+    EXPECT_CALL(img, set_pixel(2,9,255)).Times(1);
+    Renderer rend;
+    rend.draw_line(img,
+                   2,8, 
+                   2,9,
+                   255);
+}
